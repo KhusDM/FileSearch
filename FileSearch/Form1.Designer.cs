72e9e982d8974f3f7386cc4d4c0759b275d7c333
Dmitry <d.xusnetd@yandex.ru>
2019-09-15 12:47:50 +0300

    1 commit

---
+++ FileSearch/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace FileSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(268, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(365, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(268, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(365, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(268, 64);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(365, 122);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(265, 203);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Текущий файл:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(265, 233);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Обработанные файлы:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(265, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Время:";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(217, 426);
+            this.richTextBox2.TabIndex = 7;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(666, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Начать поиск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(666, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 23);
+            this.button2.TabIndex = 9;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(351, 203);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(394, 233);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(315, 265);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(841, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ FileSearch/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+
+namespace FileSearch
+{
+    enum SearchState
+    {
+        Begin,
+        Stop,
+        Continue,
+        Done
+    };
+
+    public partial class Form1 : Form
+    {
+        private SearchState searchState;
+        private string searchPattern = "";
+        private int processedFiles = 0;
+        private CancellationTokenSource cts;
+        private CancellationToken token;
+        private ManualResetEventSlim limiter;
+        private Stopwatch stopwatch;
+        private Task searchTask;
+
+        private List<string> FilesFound { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            FilesFound = new List<string>();
+            searchState = SearchState.Begin;
+            limiter = new ManualResetEventSlim(true);
+            stopwatch = new Stopwatch();
+            timer1.Enabled = true;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            switch (searchState)
+            {
+                case SearchState.Begin:
+                    searchPattern = "*" + textBox2.Text + "*";
+                    cts = new CancellationTokenSource();
+                    token = cts.Token;
+                    stopwatch.Restart();
+                    DirSearchAsync(textBox1.Text, token);
+                    button1.Text = "Остановить поиск";
+                    button2.Text = "Начать новый поиск";
+                    button2.Enabled = true;
+                    searchState = SearchState.Stop;
+                    break;
+                case SearchState.Stop:
+                    limiter.Reset();
+                    stopwatch.Stop();
+                    button1.Text = "Продолжить поиск";
+                    searchState = SearchState.Continue;
+                    break;
+                case SearchState.Continue:
+                    limiter.Set();
+                    stopwatch.Start();
+                    button1.Text = "Остановить поиск";
+                    searchState = SearchState.Stop;
+                    break;
+                case SearchState.Done:
+                    stopwatch.Stop();
+                    button1.Text = "";
+                    button1.Enabled = false;
+                    searchState = SearchState.Begin;
+                    break;
+                default: break;
+            }
+        }
+
+        //private void DirSearch(string startDirectory, CancellationToken token)
+        //{
+        //    try
+        //    {
+        //        foreach (string directory in Directory.GetDirectories(startDirectory))
+        //        {
+        //            foreach (string file in Directory.GetFiles(directory, searchPattern))
+        //            {
+        //                string fileText = "";
+        //                using (FileStream fileStream = new FileStream(file, FileMode.Open))
+        //                {
+        //                    label4.Text = file;
+        //                    using (StreamReader streamReader = new StreamReader(fileStream))
+        //                    {
+        //                        if (token.IsCancellationRequested) return;
+        //                        limiter.Wait();
+
+        //                        fileText = streamReader.ReadToEnd();
+        //                    }
+        //                }
+
+        //                if (fileText.Contains(richTextBox1.Text))
+        //                    FilesFound.Add(file);
+
+        //                processedFiles++;
+        //                label5.Text = processedFiles.ToString();
+        //            }
+
+        //            DirSearch(directory, token);
+        //        }
+        //    }
+        //    catch (System.Exception excpt)
+        //    {
+        //        Console.WriteLine(excpt.Message);
+        //    }
+        //}
+
+        private void DirSearch(string startDirectory, CancellationToken token)
+        {
+            try
+            {
+                foreach (string file in Directory.GetFiles(startDirectory, searchPattern))
+                {
+                    string fileText = "";
+                    using (FileStream fileStream = new FileStream(file, FileMode.Open))
+                    {
+                        label4.Text = file;
+                        using (StreamReader streamReader = new StreamReader(fileStream))
+                        {
+                            if (token.IsCancellationRequested) return;
+                            limiter.Wait();
+
+                            fileText = streamReader.ReadToEnd();
+                        }
+                    }
+
+                    if (fileText.Contains(richTextBox1.Text))
+                        FilesFound.Add(file);
+
+                    processedFiles++;
+                    label5.Text = processedFiles.ToString();
+                }
+
+                foreach (string directory in Directory.GetDirectories(startDirectory))
+                {
+                    DirSearch(directory, token);
+                }
+            }
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+        }
+
+        private async void DirSearchAsync(string startDirectory, CancellationToken token)
+        {
+            if (token.IsCancellationRequested) return;
+
+            await Task.Run(() => DirSearch(startDirectory, token));
+            searchState = SearchState.Done;
+            Button1_Click(new object(), new EventArgs());
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            cts.Cancel();
+            if (!limiter.IsSet)
+                limiter.Set();
+
+            FilesFound.Clear();
+            processedFiles = 0;
+            label4.Text = "";
+            button1.Enabled = true;
+            searchState = SearchState.Begin;
+
+            Button1_Click(sender, e);
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            TimeSpan timeSpan = stopwatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+              timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds,
+              timeSpan.Milliseconds / 10);
+
+            label6.Text = elapsedTime;
+        }
+    }
+}
