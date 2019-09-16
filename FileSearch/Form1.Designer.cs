a7efd45b9b096396ee2590a62fe816a8cc9ef78b
Dmitry <d.xusnetd@yandex.ru>
2019-09-16 15:03:41 +0300

    3 commit

--- FileSearch/Form1.Designer.cs
+++ FileSearch/Form1.Designer.cs
@@ -35,13 +35,13 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
             this.SuspendLayout();
             // 
             // textBox1
@@ -93,14 +93,6 @@
             this.label3.TabIndex = 6;
             this.label3.Text = "Время:";
             // 
-            // richTextBox2
-            // 
-            this.richTextBox2.Location = new System.Drawing.Point(12, 12);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(217, 426);
-            this.richTextBox2.TabIndex = 7;
-            this.richTextBox2.Text = "";
-            // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(666, 9);
@@ -123,6 +115,7 @@
             // 
             // timer1
             // 
+            this.timer1.Interval = 1;
             this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
             // 
             // label4
@@ -149,17 +142,25 @@
             this.label6.Size = new System.Drawing.Size(0, 13);
             this.label6.TabIndex = 12;
             // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(231, 450);
+            this.treeView1.TabIndex = 13;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(841, 450);
+            this.Controls.Add(this.treeView1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -167,7 +168,7 @@
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "File Search";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -182,13 +183,13 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.RichTextBox richTextBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TreeView treeView1;
     }
 }
 
--- FileSearch/Form1.cs
+++ FileSearch/Form1.cs
@@ -1,10 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
@@ -31,8 +26,6 @@ namespace FileSearch
         private ManualResetEventSlim limiter;
         private Stopwatch stopwatch;
 
-        private List<string> FilesFound { get; set; }
-
         public Form1()
         {
             InitializeComponent();
@@ -41,8 +34,6 @@ namespace FileSearch
             limiter = new ManualResetEventSlim(true);
             stopwatch = new Stopwatch();
             timer1.Enabled = true;
-
-            FilesFound = new List<string>();
 
             var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
             textBox1.Text = config.AppSettings.Settings["directory"].Value;
@@ -55,7 +46,9 @@ namespace FileSearch
             switch (searchState)
             {
                 case SearchState.Begin:
-                    searchPattern = "*" + textBox2.Text + "*";
+                    if (!String.IsNullOrEmpty(textBox2.Text) && !String.IsNullOrWhiteSpace(textBox2.Text))
+                        searchPattern = "*" + textBox2.Text + "*";
+
                     cts = new CancellationTokenSource();
                     token = cts.Token;
                     stopwatch.Restart();
@@ -91,7 +84,8 @@ namespace FileSearch
                     using (FileStream fileStream = new FileStream(file, FileMode.Open))
                     {
                         if (token.IsCancellationRequested) return;
-                        label4.Text = file;
+
+                        BeginInvoke(new Action(delegate { label4.Text = file; }));
                         using (StreamReader streamReader = new StreamReader(fileStream))
                         {
                             if (token.IsCancellationRequested) return;
@@ -105,17 +99,18 @@ namespace FileSearch
                     {
                         if (token.IsCancellationRequested) return;
 
-                        FilesFound.Add(file);
+                        BeginInvoke(new Action(delegate { UpdateTree(file, token); }));
                     }
 
-                    processedFiles++;
                     if (token.IsCancellationRequested) return;
-                    label5.Text = processedFiles.ToString();
+                    processedFiles++;
+                    BeginInvoke(new Action(delegate { label5.Text = processedFiles.ToString(); }));
                 }
 
                 foreach (string directory in Directory.GetDirectories(startDirectory))
                 {
                     if (token.IsCancellationRequested) return;
+
                     DirSearch(directory, token);
                 }
             }
@@ -132,11 +127,31 @@ namespace FileSearch
                 if (!token.IsCancellationRequested)
                 {
                     stopwatch.Stop();
-                    button1.Text = "";
-                    button1.Enabled = false;
+                    BeginInvoke(new Action(delegate { button1.Text = ""; button1.Enabled = false; }));
+                    //BeginInvoke(new Action(delegate { button1.Enabled = false; }));
                     searchState = SearchState.Begin;
                 }
             }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        private void UpdateTree(string path, CancellationToken token)
+        {
+            if (token.IsCancellationRequested) return;
+
+            var childs = treeView1.Nodes;
+            var parts = path.Split('\\');
+            int n = parts.Length;
+            for (int i = 0; i < n; i++)
+            {
+                var foundNodes = childs.Find(parts[i], false);
+
+                if (token.IsCancellationRequested) return;
+
+                if (foundNodes.Length > 0)
+                    childs = foundNodes[0].Nodes;
+                else
+                    childs = childs.Add(parts[i], parts[i]).Nodes;
+            }
         }
 
         private void Button2_Click(object sender, EventArgs e)
@@ -150,9 +165,11 @@ namespace FileSearch
                 searchState = SearchState.Begin;
             }
 
-            FilesFound.Clear();
-            processedFiles = 0;
+            searchPattern = "";
             label4.Text = "";
+            processedFiles = 0;
+            label5.Text = "";
+            treeView1.Nodes.Clear();
             button1.Enabled = true;
 
             Button1_Click(sender, e);
@@ -161,17 +178,9 @@ namespace FileSearch
         private void Timer1_Tick(object sender, EventArgs e)
         {
             TimeSpan timeSpan = stopwatch.Elapsed;
-            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-              timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds,
-              timeSpan.Milliseconds / 10);
-
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds,
+                timeSpan.Milliseconds / 10);
             label6.Text = elapsedTime;
-
-            string tree = "";
-            foreach (var file in FilesFound)
-            {
-                //re
-            }
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
